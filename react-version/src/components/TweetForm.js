c0f01b7aed5a1e74947a8302084bd6379b31ee71
Nika Ptushkina <99292412+nika-ma@users.noreply.github.com>
2022-05-16 22:49:54 -0700

    Convert html content

--- react-version/src/components/TweetForm.js
+++ react-version/src/components/TweetForm.js
@@ -1,11 +1,12 @@
 import React from 'react'; //optional
 
 function TweetForm(){
+  const hummingText = "What are you humming about?";
   return (
     <main className="container">
     <section className="newtweet">
       <form method="post" action="/tweets" className="newtweet__form">
-        <textarea className="form__textarea" name="text" placeholder="What are you humming about?"></textarea>
+        <textarea className="form__textarea" name="text" placeholder={hummingText}></textarea>
         <input type="submit" value="Tweet" className="form__input"/>
         <span className="form__counter">140</span>
       </form>
--- react-version/src/components/TweetList.js
+++ react-version/src/components/TweetList.js
@@ -1,21 +1,31 @@
 import React from 'react'; //optional
 
 function TweetList(){
+  const tweet = {
+    avatar: "https://i.imgur.com/73hZDYK.png",
+    firstName: "Isaac",
+    lastName: "Newton",
+    handle: "@SirIsaac",
+    body: "If I have seen further it is by standing on the shoulders of giants",
+    age: "10 days ago",
+  }
+
   return (
     <main className="container">
     <section className="tweets">
       <article className="tweet">
         <header className="tweet--header">
-          <img className="tweet--avatar" src="https://i.imgur.com/73hZDYK.png"/>
-          <h2 className="tweet--name">Newton</h2>
-          <small className="tweet--handle">@SirIsaac</small>
+          <img className="tweet--avatar" src={tweet.avatar} alt="" />
+          <h2 className="tweet--name">{tweet.lastName}</h2>
+          <small className="tweet--handle">{tweet.handle}</small>
         </header>
 
         <div className="tweet--body">
-          <p>If I have seen further it is by standing on the shoulders of giants</p>
+          <p>{tweet.body}</p>
         </div>
+
         <footer className="tweet--footer">
-          <small className="footer--age">10 days ago<small>
+          <small className="footer--age">{tweet.age}<small>
               <span className="footer--actions">
                 <a href="#"><i className="fa fa-flag"></i></a>
                 <a href="#"><i className="fa fa-retweet"></i></a>
@@ -25,15 +35,17 @@ function TweetList(){
       </article>
       <article className="tweet">
         <header className="tweet--header">
-          <img className="tweet--avatar" src="https://i.imgur.com/nlhLi3I.png"/>
-          <h2 className="tweet--name">Descartes</h2>
-          <small className="tweet--handle">@rd</small>
+          <img className="tweet--avatar" src={tweet.avatar} alt="" />
+          <h2 className="tweet--name">{tweet.lastName}</h2>
+          <small className="tweet--handle">{tweet.handle}</small>
         </header>
+
         <div className="tweet--body">
-          <p>Je pense , donc je suis</p>
+          <p>{tweet.body}</p>
         </div>
+
         <footer className="tweet--footer">
-          <small className="footer--age">10 days ago<small>
+          <small className="footer--age">{tweet.age}<small>
               <span className="footer--actions">
                 <a href="#"><i className="fa fa-flag"></i></a>
                 <a href="#"><i className="fa fa-retweet"></i></a>
